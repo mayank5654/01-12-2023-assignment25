81018efccd631dcb56c67d169921790ff02d3d3d
mayank5654 <mayankkaushal99@gmail.com>
2023-12-01 20:25:11 +0530

    Add README file

---
+++ MainApp/MainApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                // Demonstrate using the shared library in MainApp
+
+                // Log a message
+                MySharedLibrary.Utilities.LogMessage("Application started.");
+
+                // Validate an email
+                string email = "user@example.com";
+                bool isValidEmail = MySharedLibrary.Utilities.ValidateEmail(email);
+
+                Console.WriteLine($"Is {email} a valid email? {isValidEmail}");
+
+                // Additional modules can be added to demonstrate more functionalities
+            
+        }
+    }
+
+}
---
+++ MySharedLibrary/MySharedLibrary/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MySharedLibrary
+{
+    public class Utilities
+    {
+        public static void LogMessage(string message)
+        {
+            Console.WriteLine($"[Log] {message}");
+        }
+
+        public static bool ValidateEmail(string email)
+        {
+            // Simplified email validation for demonstration
+            return email.Contains("@");
+        }
+    }
+}   
+
+
+
